f59d19a76c058739f1c01b93762898d0faf7dcd7
maxim-polyakov <maxim7012@mail.ru>
2025-03-04 20:33:39 +0300

    server

---
+++ server/index.js
@@ -0,0 +1,37 @@
+
+require('dotenv').config()
+const express = require('express')
+const router = require('./src/app/routes/index')
+const db = require('./src/db/index.ts')
+const messages = require('./src/db/schema.ts')
+const PORT = process.env.PORT || 5000
+
+const app = express()
+app.use(express.json());
+app.use('/api', router)
+
+app.get("/api/", async function(req, res){
+    const data = await db.db
+        .select()
+        .from(messages.mymessages)
+    res.send(data);
+});
+
+app.post("/api/", async function (req, res) {
+    const out = await db.db.insert(messages.mymessages).values({
+        message:req.body['value'],
+    })
+    res.send(out);
+});
+
+
+const start = async () => {
+    try {
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+    } catch (e) {
+        console.log(e)
+    }
+}
+
+
+start()
---
+++ server/src/db/migrations/0000_spicy_slyde.sql
@@ -0,0 +1,3 @@
+CREATE TABLE "mymessages" (
+	"message" varchar NOT NULL
+);
