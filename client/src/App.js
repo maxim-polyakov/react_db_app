0e04574e0d3e52b43a236971de9f007c0cf26210
maxim-polyakov <maxim7012@gmail.com>
2025-03-15 01:45:17 +0300

    styles

--- client/src/App.js
+++ client/src/App.js
@@ -11,13 +11,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={`${process.env.REACT_APP_API_URL}/${count}.jpg`}
-             width={300}
-             height={300}/>
-          <input type="text" placeholder="Message" name="item_number" required onChange={(e)=>setValue(e.target.value)}/>
-          <button onClick={()=>create({value})}>Send POST</button>
-          <button onClick={()=>setSvalue(fetch())}>Get POST</button>
-          <button onClick={()=>setCount(count + 1)}>Get Count</button>
+        <img src={`${process.env.REACT_APP_API_URL}/${count}.jpg`} width={300} height={300}/>
+          <input type="text" placeholder="Message" name="item_number" required onChange={(e)=>setValue(e.target.value)} className="input"/>
+          <button onClick={()=>create({value})} type="button"  className="button">Send POST</button>
+          <button onClick={()=>setSvalue(fetch())} type="button"  className="button">Get POST</button>
+          <button onClick={()=>setCount(count + 1)} type="button"  className="button">Set Count</button>
           <output>{svalue}</output>
         <div className="video-section">
           <video src={`${process.env.REACT_APP_API_URL}/${3}.mp4`}
